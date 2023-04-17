e5572a23a6137770ce728fc7b9d24854e7a9ea79
Hachimi <hachimi.bouizegarene@outlook.fr>
2023-04-17 16:09:32 +0200

    style de la page d'inscription

--- views/ViewInscription.php
+++ views/ViewInscription.php
@@ -1,34 +1,47 @@
-<div class="page">
+
     <div class="container">
         <h1>Inscription</h1>
         <form method="POST">
-            <label for="prenom">Prenom *</label>
-            <input id="prenom" name="prenom"/>
+            <div>
+                <label for="prenom">Prenom *</label>
+                <input id="prenom" name="prenom"/>
+            </div>
 
-            <label for="nom">Nom *</label>
-            <input id="nom" name="nom"/>
+            <div>
+                <label for="nom">Nom *</label>
+                <input id="nom" name="nom"/>
+            </div>
+            <div>
+                <label for="sexe">Civilité *</label>
+                <select id="sexe" name="sexe">
+                    <option value="homme">Homme</option>
+                    <option value="femme">Femme</option>
+                    <option value="non-binaire">Non-precise</option>
+                </select>
+            </div>
+            <div>
+                <label for="dateNaiss">Date de naissance *</label>
+                <input id="dateNaiss" name="dateNaiss"/>
+            </div>
+            <div>
+                <label for="mail">Mail *</label>
+                <input id="mail" name="mail"/>
+            </div>
 
-            <label for="sexe">Civilité *</label>
-            <select id="sexe" name="sexe">
-                <option value="homme">Homme</option>
-                <option value="femme">Femme</option>
-                <option value="non-binaire">Non-precise</option>
-            </select>
+            <div>
+                <label for="mdp">Mot de passe *</label>
+                <input id="mdp" name="mdp"/>
+            </div>
+            <div>
+                <label for="mdpConf">Confirmer mot de passe *</label>
+                <input id="mdpConf" name="mdpConf"/>
+            </div>
 
-            <label for="dateNaiss">Date de naissance *</label>
-            <input id="dateNaiss" name="dateNaiss"/>
+            
 
-            <label for="mail">Mail *</label>
-            <input id="mail" name="mail"/>
-
-            <label for="mdp">Mot de passe *</label>
-            <input id="mdp" name="mdp"/>
-
-            <label for="mdpConf">Mot de passe *</label>
-            <input id="mdpConf" name="mdpConf"/>
+            
             
             <button type="submit">Valider</button>
             
         </form>
     </div>
-</div>
